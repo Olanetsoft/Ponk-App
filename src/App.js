9e8002527a2e5b27a0432e24f9e0503787662762
olanetsoft <heedris2olubisi@gmail.com>
2019-12-03 13:54:32 +0100

    app.js modified

--- src/App.js
+++ src/App.js
@@ -6,20 +6,20 @@ class App extends Component {
   constructor(){
       super();
       this.state ={
-        monster:[]
+        monsters:[]
       }
     }
 
     componentDidMount(){
       fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
-      .then(users => this.setState({monster: users}))
+      .then(users => this.setState({monsters: users}))
     }
   
     render() {
       return (
         <div className="App">
-          <CardList monsters={this.state.monster}/>
+          <CardList monsters={this.state.monsters}/>
       </div>
       );
     }
