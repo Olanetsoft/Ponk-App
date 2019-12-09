afe05bad0d8792780957e46a3b631cd9c034574d
olanetsoft <heedris2olubisi@gmail.com>
2019-12-09 11:54:31 +0100

    Updates

---
+++ precache-manifest.9bcc607e63a5e500c7fe44007653304a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fa09c6fff85dbe58f3fb9fcd1613e72",
+    "url": "/ponk-app/index.html"
+  },
+  {
+    "revision": "1d253ad054f7b68e4e8a",
+    "url": "/ponk-app/static/css/main.8463f2b9.chunk.css"
+  },
+  {
+    "revision": "ccaef061de66b81aefca",
+    "url": "/ponk-app/static/js/2.01e135a8.chunk.js"
+  },
+  {
+    "revision": "1d253ad054f7b68e4e8a",
+    "url": "/ponk-app/static/js/main.6a54ebec.chunk.js"
+  },
+  {
+    "revision": "69228c07d8c11deb137b",
+    "url": "/ponk-app/static/js/runtime-main.de2b21a9.js"
+  }
+]);
